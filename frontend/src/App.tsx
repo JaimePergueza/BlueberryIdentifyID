a6b218ff5b0a3806f41f3b17c8de733a057e7ec3
PERGUEZA HERNANDEZ JAIME ALEXANDER <jaime.pergueza@upec.edu.ec>
2026-07-31 00:26:45 -0500

    feat(frontend): wire the complete MVP route flow

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { Navigate, Route, Routes } from "react-router";
+import { AppShell } from "./components/AppShell";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AnalysisDetailPage } from "./pages/AnalysisDetailPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { HistoryPage } from "./pages/HistoryPage";
+import { LoginPage } from "./pages/LoginPage";
+import { NewAnalysisPage } from "./pages/NewAnalysisPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { PreliminaryResultPage } from "./pages/PreliminaryResultPage";
+import { ReviewPage } from "./pages/ReviewPage";
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        element={
+          <ProtectedRoute roles={["admin", "specialist"]}>
+            <AppShell />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<DashboardPage />} />
+        <Route path="analyses" element={<HistoryPage />} />
+        <Route path="analyses/new" element={<NewAnalysisPage />} />
+        <Route path="analyses/:analysisRunId" element={<AnalysisDetailPage />} />
+        <Route path="analyses/:analysisRunId/preliminary" element={<PreliminaryResultPage />} />
+        <Route path="analyses/:analysisRunId/review" element={<ReviewPage />} />
+        <Route path="404" element={<NotFoundPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/404" replace />} />
+    </Routes>
+  );
+}
